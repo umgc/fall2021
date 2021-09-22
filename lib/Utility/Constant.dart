2cccb34a824d3a6e34a116c7ef4b7d3cc2fd1244
Daniel <daniel@danielavery.net>
2021-09-22 08:54:20 -0400

    more translations

--- lib/Utility/Constant.dart
+++ lib/Utility/Constant.dart
@@ -2,16 +2,6 @@ import 'dart:ui';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class SCREEN_NAMES {
-   static final String MENU = "MENU";
-   static final String NOTE = "NOTE";
-   static final String HOME = "HOME";
-   static final String NOTIFICATION = "NOTIFICATION";
-   static final String SETTING = "SETTING";
-   static final String CALENDAR = "CALENDAR";
-
- }
-
  const SUPPORTED_LOCALES = [Locale('en', 'US'), Locale('es', 'US')];
 
 const LOCALIZATION_DELEGATES = [
