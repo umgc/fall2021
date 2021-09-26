b8472455c88881495a70f08637bb265ad227bb42
Daniel <daniel@danielavery.net>
2021-09-26 19:33:37 -0400

    remove some unused code

--- lib/Screens/Main.dart
+++ lib/Screens/Main.dart
@@ -12,7 +12,6 @@ import 'NotificationScreen.dart';
 import 'Menu.dart';
 import './Note/SaveNote.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_search_bar/flutter_search_bar.dart';
 
 import 'package:flutter_mobx/flutter_mobx.dart';
--- lib/Utility/Constant.dart
+++ lib/Utility/Constant.dart
@@ -1,23 +1,21 @@
 import 'dart:ui';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SCREEN_NAMES {
-   static final String MENU = "MENU";
-   static final String SAVE_NOTE = "SAVE_NOTE";
-   static final String NOTE = "NOTE";
-   static final String HOME = "HOME";
-   static final String NOTIFICATION = "NOTIFICATION";
-   static final String SETTING = "SETTING";
-   static final String CALENDAR = "CALENDAR";
-   static final String NOTE_DETAIL = "NOTE_DETAIL";
- }
+  static final String MENU = "MENU";
+  static final String SAVE_NOTE = "SAVE_NOTE";
+  static final String NOTE = "NOTE";
+  static final String HOME = "HOME";
+  static final String NOTIFICATION = "NOTIFICATION";
+  static final String SETTING = "SETTING";
+  static final String CALENDAR = "CALENDAR";
+  static final String NOTE_DETAIL = "NOTE_DETAIL";
+}
 
- const SUPPORTED_LOCALES = [Locale('en', 'US'), Locale('es', 'US')];
+const SUPPORTED_LOCALES = [Locale('en', 'US'), Locale('es', 'US')];
 
 const LOCALIZATION_DELEGATES = [
-      AppLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-       GlobalWidgetsLocalizations.delegate,
-       GlobalCupertinoLocalizations.delegate,
+  GlobalMaterialLocalizations.delegate,
+  GlobalWidgetsLocalizations.delegate,
+  GlobalCupertinoLocalizations.delegate,
 ];
