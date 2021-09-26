5167949e2c5a72202f7fa9538dfa8978914fb477
Daniel <daniel@danielavery.net>
2021-09-26 19:35:20 -0400

    more

--- lib/Utility/Constant.dart
+++ lib/Utility/Constant.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
 class SCREEN_NAMES {
   static final String MENU = "MENU";
   static final String SAVE_NOTE = "SAVE_NOTE";
