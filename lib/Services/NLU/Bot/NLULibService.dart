c53119a9ea3081b02646705f4e3b5d114dc2a524
lsubramaniam <leelakrishnan.subramaniam@gmail.com>
2021-09-30 19:29:56 -0400

    semantic_search: Got notes from Saved notes

--- lib/Services/NLU/Bot/NLULibService.dart
+++ lib/Services/NLU/Bot/NLULibService.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 
 import 'package:untitled3/Model/NLUAction.dart';
 import 'package:untitled3/Model/NLUResponse.dart';
+import 'package:untitled3/Model/Note.dart';
 
+import '../../NoteService.dart';
 import '../BertQA/BertQaService.dart';
 import 'LexService.dart';
 
@@ -31,19 +33,37 @@ import 'LexService.dart';
       return new NLUResponse(NLUAction.Answer, message, response);
     }
 
-    String searchNotes(String message) {
-      // ToDO get Notes
-      String answer = bertQAService
-          .answer(
-          "I have a toothache. I have an appointment with "
-              "dentist on monday oct 5th at 9 am. My wife blood pressure is 138."
-              " My blood pressure is 140 above."
-              " I have a dinner on octobher 5, Raj, DJ are coming.",
-          message)
-          .first
-          .text;
+    Future<String> searchNotes(String message) async {
+      String answer = "";
+      String notes = await getNotes();
+      if (notes != null && notes != "") {
+        answer = bertQAService
+            .answer(notes,
+            message)
+            .first
+            .text;
+      }
       return answer;
-      //return "";
     }
 
+    Future<String> getNotes() async {
+      List<TextNote> lstTextNote = await TextNoteService.loadNotes();
+      var strBufffer = new StringBuffer();
+      String notes = "";
+      if (lstTextNote != null && lstTextNote.length > 0) {
+        for (int i = 0; i < lstTextNote.length; i++) {
+          TextNote textNote = lstTextNote.elementAt(i);
+          if (textNote != null && textNote.noteId != null
+              && textNote.noteId != "" && textNote.text != null
+              && textNote.text != "") {
+            strBufffer.write(textNote.text);
+          }
+        }
+      }
+      if (strBufffer != null && strBufffer.toString() != null &&
+          strBufffer.toString() != "") {
+        notes = strBufffer.toString();
+      }
+      return notes;
+    }
   }
