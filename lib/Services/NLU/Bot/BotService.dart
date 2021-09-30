15fb4e355a16ac54635aab32a6946362a8413d2b
lsubramaniam <leelakrishnan.subramaniam@gmail.com>
2021-09-29 20:33:03 -0400

    semantic_search: Included tensorflow Bert QA integration to the project.

--- lib/Services/NLU/Bot/BotService.dart
+++ lib/Services/NLU/Bot/BotService.dart
@@ -25,8 +25,8 @@ enum MessageFormat { PlainText, CustomPayload, SSML, Composite }
     late Map<String, dynamic> result;
     String botName = "BookAppointment";
     String kAccessKeyId = 'ASIAZWJRDCYYKA6INEEW';
-    String kSecretAccessKey = 'kZqma4VZie1awlEqLPgVOQzVl2qiQOV+WJH86zwR';
-    String sessionToken = 'IQoJb3JpZ2luX2VjEDoaCXVzLWVhc3QtMiJIMEYCIQDQh4gelDqno96q39RwiPT5x7K7SyVOSmeDpUMd9SthWAIhAP5tT81Cb+Rb2zN85delmYB4KECmW1uL7Tr36C/M2GaJKr0DCKP//////////wEQARoMNjY2MzU5NzY0NTI4Igyu9F2yAqZN3dG0q9YqkQMVrg/4mCJjDxg0QmplU581Z2P8LGhGfr9vgei6SaONhhfks5Kt9Ikbh61G9UiQ3SXgPLbHjOfTUueaIIcBz1Y3LcW+WajtfsGfB8CqT76lkJLtkvl+1KjSCVn6k+/K/iWgr3Zc1Ej+qT2djTH4x1OWFNS6i6iCtlUy/Z6i3P2fziHGsEmafkH3ict+07dFb3DA2aRnUhnaCHfQDNd/5ub70oILwB4UgtgGNkbM9SE/NxKgPZY9qIktYifqcgfDyYMYHlvY9XEc0UT2jfaQKDYVgMCdsdsW5mkoBYzLRisQhKxjfwaBpkRtdW8dEHFAG04eV4JSAbOSat3bgUwahATGizOdsMz/qhnS9qzShQGgSR6OU6pDDUtuHCGh0sgwrjsZ+bGDfzkw5Sy3JhjQpozfinCsAmDZ1t3nX6llw9OR9B2mdDHCeccsWGwjIvmprs21FtgjDuKGzaAET6HgQAR+pkFUgxBWVmZArtck1ziG21FEN8pFR75rOgxSkQ3yEZeDZkIIZ/aJnABGvbC3Fbq9ATD6ycuKBjqlAaGPeFKzdCR1dBh4sHQVHejXNegWWZV72n4MLyZx2FE9wLUfPGXXW+pYZg4SySvN0Z4OnGoYdlO/pjKvdRa507mSD8N8EhkwgpJMatFobJb0hsz7GY5flutVSkDfBDYkU91vpl7YCJ5rlvuR0I6iWe+K7smYj5hzm16YokWsRQ4EeWHo0peEJuqTZrZt/U4gHVsFpG44V8Yb6iRdZL78E+5xcgjeFw==';
+    String kSecretAccessKey = '';
+    String sessionToken = '';
     String botAlias = "BookAppointmentDeployment";
     String botAWSRegion = "us-east-1";
     late final BertQAService bertQAService;
