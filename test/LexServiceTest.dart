4a4b8d174817506cefec1c461a3bfb817d9b4c36
lsubramaniam <leelakrishnan.subramaniam@gmail.com>
2021-10-01 07:43:15 -0400

    semantic_search: Fixed integration text.

--- test/LexServiceTest.dart
+++ test/LexServiceTest.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:untitled3/Services/NLU/Bot/LexService.dart';
+import 'package:untitled3/Services/NoteService.dart';
 
 void main() async {
   try {
     final lex = LexService();
-    Map<String, dynamic> value = await lex.getLexResponse(text: 'how do I create a note');
+    String sessionId = TextNoteService.generateUUID();
+    Map<String, dynamic> value = await lex.getLexResponse(text: 'how do I create a note', locale: 'en-US', userId: sessionId);
     if (value != null) {
       expect(value.length > 0, true);
     }
