6cb3f3fa646eb6f52674da1015c2036f6ce6379d
prestech <muwanpresley@gmail.com>
2021-10-01 23:51:59 -0400

    onbaording screen

--- lib/Screens/Onboarding/Boarding.dart
+++ lib/Screens/Onboarding/Boarding.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:untitled3/Model/Setting.dart';
+import 'package:untitled3/Observables/OnboardObservable.dart';
+import 'package:untitled3/Observables/SettingObservable.dart';
+import 'package:untitled3/Screens/Main.dart';
+import 'package:untitled3/Screens/Onboarding/CloudSetup.dart';
+import 'package:untitled3/Screens/Onboarding/Permission.dart';
+import 'package:untitled3/Screens/Onboarding/SelectLanguage.dart';
+import 'package:untitled3/Screens/Onboarding/Walkthrough.dart';
+
+import 'package:untitled3/generated/i18n.dart';
+
 
 class OnBoardingScreen extends StatefulWidget {
   @override
@@ -7,13 +20,90 @@ class OnBoardingScreen extends StatefulWidget {
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
+  _OnBoardingScreenState();
+
+  OnboardObserver onboardObserver = OnboardObserver();
+  Setting onbaordSetting =  Setting();
+
+  static const int NUM_OF_ONBOARDING_SCREEN = 4; 
+
+  String _screenName(index){
+      List<String> boardingScreens = [
+        I18n.of(context)!.OboardLangSetup,
+        I18n.of(context)!.OboardPermissionSetup,
+        I18n.of(context)!.OboardCloudSettup,
+        I18n.of(context)!.WalkthroughScreen,
+    ];
+    return boardingScreens[index];
+   }
+
+   Widget _changeScreen(index) {
+   
+    
+    //when onboarding is completed
+    if(index > NUM_OF_ONBOARDING_SCREEN-1){
+        final settingObserver = Provider.of<SettingObserver>(context);
+        onbaordSetting.isFirstRun = false;
+
+        settingObserver.saveSetting(onbaordSetting);
+        //move to the Main screen
+         Navigator.pushReplacement<void, void>(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) =>  MainNavigator(),
+                  ));
+        
+        return Text("Welcome to Memory Magic");
+    }
+
+    String name = _screenName(index);
+
+    print("Oboarding screen: name $name  indext $index " );
+
+
+    if (name == I18n.of(context)!.OboardLangSetup) {
+      return  SelectLanguageScreen();
+    }
+    if (name == I18n.of(context)!.OboardPermissionSetup) {
+      print("Return " + name);
+      return PermissionScreen();
+    }
+    if (name == I18n.of(context)!.OboardCloudSettup) {
+      print("Return " + name);
+      return CloudSetupScreen();
+    }
+
+    return WalkthroughScreen();
+   }
+
+  AppBar buildAppBar(BuildContext context) {
+    
+    final settingObserver = Provider.of<SettingObserver>(context);
+    return AppBar(
+      
+      toolbarHeight: 90,
+      title: Observer(
+          builder: (_) => Text(
+                '${_screenName(onboardObserver.currentScreenIndex)}',
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.black),
+              )),
+      backgroundColor: Color(0xFF33ACE3),
+      centerTitle: true,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        appBar: buildAppBar(context),
         body: Center(
-          child: Text("Onbarding screen"),
+          child: Observer(
+                  builder: (_) => 
+                      _changeScreen(onboardObserver.currentScreenIndex)
+          ) 
         ),
 
         persistentFooterButtons: [
@@ -22,11 +112,11 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
           children: [
             ElevatedButton(
                     child: Text("BACK"),
-                    onPressed: null,
+                    onPressed: ()=>{onboardObserver.moveToPrevScreen()},
                   ), 
             ElevatedButton(
                     child: Text("NEXT"),
-                    onPressed: null,
+                    onPressed: ()=>{onboardObserver.moveToNextScreen()},
                   ),
 
           ])
