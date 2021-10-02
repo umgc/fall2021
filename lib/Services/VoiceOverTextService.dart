2c239fe2e3a48a2009fda795cb48f99c3c77e8db
Daniel <daniel@danielavery.net>
2021-10-02 17:56:03 -0400

    add todo

--- lib/Services/VoiceOverTextService.dart
+++ lib/Services/VoiceOverTextService.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
 class VoiceOverTextService {
-
   VoiceOverTextService();
 
+  // TODO: Add the locale as a parameter and use
+  // setLanguage feature to set locale as necessary
   static Future<dynamic> speakOutLoud(String text) async {
     FlutterTts flutterTts = FlutterTts();
     flutterTts.awaitSpeakCompletion(true);
