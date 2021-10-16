6fbb56d3f2f23e0bf6e76cc0da0b96237d3d6794
Daniel <daniel@danielavery.net>
2021-10-16 13:14:03 -0400

    updates

---
+++ lib/Screens/Splash/SplashScreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:untitled3/Observables/SettingObservable.dart';
+import 'package:untitled3/Screens/Onboarding/Boarding.dart';
+import '../Main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: (5)),
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final settingObserver = Provider.of<SettingObserver>(context);
+
+    return Scaffold(
+      body: Lottie.asset(
+        'assets/lottie/animation.json',
+        controller: _controller,
+        height: MediaQuery.of(context).size.height * 1,
+        animate: true,
+        frameRate: FrameRate(60.0),
+        onLoaded: (composition) {
+          _controller
+            ..duration = composition.duration
+            ..forward().whenComplete(() => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Observer(
+                          builder: (_) =>
+                              (settingObserver.userSettings.isFirstRun == false)
+                                  ? MainNavigator()
+                                  : (OnBoardingScreen()))),
+                ));
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
-// Official
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:untitled3/Observables/MicObservable.dart';
 import 'package:untitled3/Observables/OnboardObservable.dart';
-// Internal
 import 'package:untitled3/Screens/Note/NoteDetail.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:untitled3/Screens/Onboarding/Boarding.dart';
-
+import 'Screens/Splash/SplashScreen.dart';
 import 'Utility/FontUtil.dart';
 import 'Utility/ThemeUtil.dart';
 import 'generated/i18n.dart';
-import 'Screens/Main.dart';
 import 'package:provider/provider.dart';
-
 import 'package:untitled3/Observables/MenuObservable.dart';
 import 'package:untitled3/Observables/SettingObservable.dart';
 import 'package:untitled3/Observables/NoteObservable.dart';
@@ -54,18 +49,6 @@ class _MyAppState extends State<MyApp> {
             backgroundColor:
                 themeToColor(settingObserver.userSettings.appTheme));
 
-    Color getColor(Set<MaterialState> states) {
-      const Set<MaterialState> interactiveStates = <MaterialState>{
-        MaterialState.pressed,
-        MaterialState.hovered,
-        MaterialState.focused,
-      };
-      if (states.any(interactiveStates.contains)) {
-        return themeToColor(settingObserver.userSettings.appTheme);
-      }
-      return Colors.black;
-    }
-
     return Observer(
         builder: (_) => MultiProvider(
                 providers: [
@@ -78,11 +61,7 @@ class _MyAppState extends State<MyApp> {
                 ],
                 child: (MaterialApp(
                   debugShowCheckedModeBanner: false,
-                  home: Observer(
-                      builder: (_) =>
-                          (settingObserver.userSettings.isFirstRun == false)
-                              ? MainNavigator()
-                              : (OnBoardingScreen())),
+                  home: SplashScreen(),
                   localizationsDelegates: [
                     i18n,
                     GlobalMaterialLocalizations.delegate,
