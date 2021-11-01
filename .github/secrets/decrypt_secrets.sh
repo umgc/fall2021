dfdcfdd78deeaf412095064165ad294bdbaaf7d3
mset1 <matt.setiawan@yahoo.com>
2021-10-31 20:55:21 -0400

    Debugging decrypt secrets

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,5 +1,1 @@
 #!/bin/sh
-
-set -eo pipefail
-
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/match_Apple-Store-UMGC_Profile.mobileprovision ./.github/secrets/match_Apple-Store-UMGC_Profile.mobileprovision.gpg
