9113a2cdd5a1e4273a0d3a86929c92c495015899
mset1 <matt.setiawan@yahoo.com>
2021-10-31 23:02:00 -0400

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export GPG_TTY=$(tty)
 gpg --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/TestGPG_output.txt ./.github/secrets/TestGPG.gpg
 
 cat ./.github/secrets/TestGPG_output.txt
