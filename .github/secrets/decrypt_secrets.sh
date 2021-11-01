f08dabe5bbdb5f3191f6ea8331797570c7bda641
mset1 <matt.setiawan@yahoo.com>
2021-11-01 19:38:22 -0400

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 gpg --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/TestGPG_output.txt ./.github/secrets/TestGPG.gpg
 
 cat ./.github/secrets/TestGPG_output.txt
