be2c4a853f79dd23e79fcdcb136c2fd21f646953
mset1 <matt.setiawan@yahoo.com>
2021-10-31 22:52:23 -0400

    Using secretrs

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gpg --decrypt --passphrase="test" --output ./.github/secrets/TestGPG_output.txt ./.github/secrets/TestGPG.gpg
+gpg --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/TestGPG_output.txt ./.github/secrets/TestGPG.gpg
 
 cat ./.github/secrets/TestGPG_output.txt
