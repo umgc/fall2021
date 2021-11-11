84b80d9f2891d1b23e30633c0081cc3a37fbce33
mset1 <matt.setiawan@yahoo.com>
2021-11-11 10:34:45 -0500

    Rearranged security imports

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -15,10 +15,12 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/Apple-Store-UMGC_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Apple-Store-UMGC_Profile.mobileprovision
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security import ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
