508a83dc3251dfa0be6f5f5e634db42e6ec2ffea
mset1 <matt.setiawan@yahoo.com>
2021-11-11 10:11:57 -0500

    Putting in the rest of decrypt secrets

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -8,17 +8,17 @@
 
 #echo RELOADAGENT | gpg-connect-agent
 echo "$IOS_KEYS" | gpg --batch --yes --passphrase-fd 0 --output ./.github/secrets/Apple-Store-UMGC_Profile.mobileprovision --decrypt ./.github/secrets/Apple-Store-UMGC_Profile.mobileprovision.gpg
-#echo "$IOS_KEYS" | gpg --batch --yes --passphrase-fd 0 --output ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12.gpg
+echo "$IOS_KEYS" | gpg --batch --yes --passphrase-fd 0 --output ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12.gpg
 
-#mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-#cp ./.github/secrets/Apple-Store-UMGC_Profile.mobilepro#vision ~/Library/MobileDevice/Provisioning\ Profiles/Apple-Store-UMGC_Profile.mobileprovision
+cp ./.github/secrets/Apple-Store-UMGC_Profile.mobilepro#vision ~/Library/MobileDevice/Provisioning\ Profiles/Apple-Store-UMGC_Profile.mobileprovision
 
-#security create-keychain -p "" build.keychain
-#security import ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Apple-Store-UMGC-ios_distribution.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
-#security list-keychains -s ~/Library/Keychains/build.keychain
-#security default-keychain -s ~/Library/Keychains/build.keychain
-#security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
-#security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
